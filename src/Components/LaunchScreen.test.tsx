4ea5c6c55a104b1befb4bf6ae697188fa27cf958
Paul Hoenecke <paulh@spindance.com>
2018-05-24 00:39:01 -0400

    watch task

--- src/Components/LaunchScreen.test.tsx
+++ src/Components/LaunchScreen.test.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
-import App from './App'
+import LaunchScreen from './LaunchScreen'
 
 import * as renderer from 'react-test-renderer'
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON()
+  const rendered = renderer.create(<LaunchScreen />).toJSON()
   expect(rendered).toBeTruthy()
 })
